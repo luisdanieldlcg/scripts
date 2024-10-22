9964f45a88ca088e201c523d4562efa3e0010bbe
luisdanieldlcg <luisdanieldlcg@gmail.com>
2024-10-22 15:38:20 -0400

    update deps

--- arch_rice.sh
+++ arch_rice.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
+echo "Installing Paru"
+sudo pacman -S --needed --noconfirm base-devel rustup
+rustup default stable
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ..
+sudo rm -r paru
+
 echo "Installing dotfiles"
 git clone https://github.com/luisdanieldlcg/dotfiles
 mv dotfiles ~/
 sh ~/dotfiles/install.sh
 
 echo "Installing dependencies"
-sudo pacman -S hyprland \
-    waybar \
-    xdg-user-dirs \
-    xdg-utils \
-    git \
-    less \
-    github-cli \
+sudo pacman -S 
+    # Hyprland stuff + status bar
+    hyprland hyprpaper waybar \
+    # Screenshots
+    grim slurp wl-clipboard \
+    # XDG tools for directories
+    xdg-user-dirs xdg-utils \
+    # Git / Github
+    git less github-cli \
     ttf-fira-code \
     alacritty \
     firefox \
-    neovim --noconfirm
+    neovim \
+    ripgrep --noconfirm
 
-echo "Setting us  directories"
+paru -S --noconfirm rofi-wayland 
+
+echo "Setting user directories"
 xdg-user-dirs-update
 
 echo "Setting up Git"
@@ -27,3 +41,4 @@ git config --global user.name "luisdanieldlcg"
 
 echo "Authenticating with github"
 gh auth login
+
